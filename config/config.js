d38c97ad2acdbe3b5d592d657c067190de1206d6
Nicole <89022581+Gabanicole@users.noreply.github.com>
2023-04-19 02:35:24 -0700

    push

--- config/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: 'your_secret_key'
-};
--- routes/siteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {getSites,createSite, getSite, updateSite, deleteSite} = require ("../controllers/siteController");
-
-router.route("/").get(getSites).post(createSite);
-router.route("/:id").get(getSite).put (updateSite).delete (deleteSite);
-
-
-module.exports= router;
