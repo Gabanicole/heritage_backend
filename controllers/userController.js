87cabd97651f1ff60975c21b6c749ee6a8e2e87d
Nicole <89022581+Gabanicole@users.noreply.github.com>
2023-04-19 22:17:38 +0200

    user

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,19 @@ const registerUser = asyncHandler(async (req,res) => {
  //hash password
  const hashedPassword = await bcrypt.hash(password, 10);
  console.log("hashed password: ", hashedPassword);
+ const user = await User.create({
+  username,
+  email,
+  password: hashedPassword,
+ });
+
+ console.log(`User created ${user}`);
+ if (user) {
+   res.status(201).json({ _id: user.id, email: user.email });
+ } else {
+   res.status(400);
+   throw new Error("User data is not valid");
+ }
  res.json({message: "register the user"});
 });
 
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/sites", require ("./routes/siteRoutes"));
 app.use("/api/blogs", require("./routes/blogRoutes"));
-app.use("/api/user", require("./routes/userRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
