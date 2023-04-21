a23d04ae8e612c4ce79beef5bf69caefd8e246ae
Nicole <89022581+Gabanicole@users.noreply.github.com>
2023-04-21 21:06:33 +0200

    role

--- models/userModel.js
+++ models/userModel.js
@@ -14,6 +14,12 @@ const userSchema = mongoose.Schema({
     type: String,
     required:[true, "please add the user password"]
    },
+   roles: 
+      {
+        type:String,
+        enum:[''],
+        default:'guest'
+      }
 },{
  timestamps:true,
 }
