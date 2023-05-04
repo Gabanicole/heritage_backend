2ab8f093cfe91f39a3b900dc44cf834cea6845fa
Nicole <89022581+Gabanicole@users.noreply.github.com>
2023-05-04 16:19:22 +0300

    push

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,12 +14,12 @@ const userSchema = mongoose.Schema({
     type: String,
     required:[true, "please add the user password"]
    },
-   roles: {
-          type: String,
-          default: "user",
-          enum: ["user", "admin", "superadmin"]
-        
-      }
+   role: {
+    type: String,
+    enum: ['admin', 'manager', 'editor', 'viewer'],
+    default: 'viewer',
+  },
+  
 },{
  timestamps:true,
 }
--- server/routes/siteRoutes.js
+++ server/routes/siteRoutes.js
@@ -7,5 +7,17 @@ router.route( "/").get (getSites).post (createSite);
 router.route( "/:id").get (getSite).put (updateSite).delete (deleteSite);
 
 
+//get site information
+router.get('/:id/edit', (req, res) => {
+ User.findById(req.params.id).then((user) => {
+   if (user.permissions.manageUsers) {
+     res.render('users/edit', { user });
+   } else {
+     res.redirect('/users');
+   }
+ });
+});
+
+
 
 module.exports = router;
